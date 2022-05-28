9f035030c611957c0fd3cd17544b7bd0d3ae4e5b
York Wong <eth2net@gmail.com>
2022-05-28 21:03:11 +0800

    null-ls as formatter

--- lua/completion/lsp-setup.lua
+++ lua/completion/lsp-setup.lua
@@ -1,60 +1,64 @@
 local M = {}
 
-
 function M.config()
-  local mappings = {
-    gd = 'lua require"telescope.builtin".lsp_definitions()',
-    gi = 'lua require"telescope.builtin".lsp_implementations()',
-    gr = 'lua require"telescope.builtin".lsp_references()',
-  }
+	local mappings = {
+		gd = 'lua require"telescope.builtin".lsp_definitions()',
+		gi = 'lua require"telescope.builtin".lsp_implementations()',
+		gr = 'lua require"telescope.builtin".lsp_references()',
+	}
 
-  local settings = {
-    -- mappings = mappings,
-    on_attach = function(client, bufnr)
-      require('nvim-lsp-setup.utils').format_on_save(client)
-      require('illuminate').on_attach(client)
-      -- TODO: check the alt key on macOS
-      -- vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
-      -- vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
-    end,
-    servers = {
-      ansiblels = {},
-      bashls = {},
-      cmake = {},
-      cssls = {},
-      dockerls = {},
-      dotls = {},
-      -- groovyls = {},
-      gopls = {},
-      html = {},
-      jsonls = {},
-      jsonnet_ls = {},
-      tsserver = {}, -- javascript
-      sumneko_lua = require('lua-dev').setup({
-        lspconfig = {
-          on_attach = function(client, _)
-            require('nvim-lsp-setup.utils').disable_formatting(client)
-          end,
-        },
-      }),
-      taplo = {}, -- toml
-      tailwindcss = {},
-      terraformls = {},
-      pyright = {},
-      rust_analyzer = {},
-      vimls = {},
-      vuels = {},
-      yamlls = {},
-    }
-  }
+	local settings = {
+		-- mappings = mappings,
+		on_attach = function(client, bufnr)
+			if client.name == "tsserver" then
+				require("nvim-lsp-setup.utils").disable_formatting(client)
+			end
+			require("nvim-lsp-setup.utils").format_on_save(client)
+			require("illuminate").on_attach(client)
+			-- TODO: check the alt key on macOS
+			-- vim.api.nvim_set_keymap('n', '<a-n>', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
+			-- vim.api.nvim_set_keymap('n', '<a-p>', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
+		end,
+		servers = {
+			ansiblels = {},
+			bashls = {},
+			cmake = {},
+			cssls = {},
+			dockerls = {},
+			dotls = {},
+			-- groovyls = {},
+			gopls = {},
+			html = {},
+			jsonls = {},
+			jsonnet_ls = {},
+			tsserver = {}, -- javascript
+			sumneko_lua = require("lua-dev").setup({
+				lspconfig = {
+					on_attach = function(client, _)
+						require("nvim-lsp-setup.utils").disable_formatting(client)
+					end,
+				},
+			}),
+			taplo = {}, -- toml
+			tailwindcss = {},
+			terraformls = {},
+			pyright = {},
+			rust_analyzer = {},
+			vimls = {},
+			vuels = {},
+			yamlls = {},
+		},
+	}
 
-  require('nvim-lsp-setup').setup(settings)
+	require("nvim-lsp-setup").setup(settings)
 
-  require('lsp_signature').setup({})
+	require("lsp_signature").setup({})
 
-  local border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }
-  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
-  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+	local border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+
+	require("completion.null-ls")
 end
 
 return M
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,185 +1,188 @@
 local M = {}
 
 _G.dump = function(...)
-  print(vim.inspect(...))
+	print(vim.inspect(...))
 end
 
 function M.setup()
-  -- Indicate first time installation
-  local packer_bootstrap = false
+	-- Indicate first time installation
+	local packer_bootstrap = false
 
-  -- packer.nvim configuration
-  local conf = {
-    profile = {
-      enable = true,
-      threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
-    },
-    display = {
-      open_fn = function()
-        return require("packer.util").float { border = "rounded" }
-      end,
-    },
-  }
+	-- packer.nvim configuration
+	local conf = {
+		profile = {
+			enable = true,
+			threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+		},
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "rounded" })
+			end,
+		},
+	}
 
-  -- Check if packer.nvim is installed
-  -- Run PackerCompile if there are changes in this file
-  local function packer_init()
-    local fn = vim.fn
-    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-      packer_bootstrap = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-      }
-      vim.cmd [[packadd packer.nvim]]
-    end
-    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
-  end
+	-- Check if packer.nvim is installed
+	-- Run PackerCompile if there are changes in this file
+	local function packer_init()
+		local fn = vim.fn
+		local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+		if fn.empty(fn.glob(install_path)) > 0 then
+			packer_bootstrap = fn.system({
+				"git",
+				"clone",
+				"--depth",
+				"1",
+				"https://github.com/wbthomason/packer.nvim",
+				install_path,
+			})
+			vim.cmd([[packadd packer.nvim]])
+		end
+		vim.cmd("autocmd BufWritePost plugins.lua source <afile> | PackerCompile")
+		-- vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerSync"
+	end
 
-  -- Plugins
-  local function plugins(use)
-    use { "wbthomason/packer.nvim" }
+	-- Plugins
+	local function plugins(use)
+		use({ "wbthomason/packer.nvim" })
 
-    -- ui
-    use {
-      -- "NLKNguyen/papercolor-theme",
-      "catppuccin/nvim",
-      config = function()
-        vim.cmd 'colorscheme catppuccin'
-      end,
-    }
+		-- ui
+		use({
+			-- "NLKNguyen/papercolor-theme",
+			"catppuccin/nvim",
+			config = function()
+				vim.cmd("colorscheme catppuccin")
+			end,
+		})
 
-    use {
-      'goolord/alpha-nvim', -- startup screen
-      requires = {
-        'kyazdani42/nvim-web-devicons',
-      },
-      config = require("ui.alpha").dashboard,
-    }
+		use({
+			"goolord/alpha-nvim", -- startup screen
+			requires = {
+				"kyazdani42/nvim-web-devicons",
+			},
+			config = require("ui.alpha").dashboard,
+		})
 
-    use {
-      'SmiteshP/nvim-gps',
-      requires = {
-        'nvim-treesitter/nvim-treesitter',
-        'windwp/nvim-autopairs',
-      },
-    }
+		use({
+			"SmiteshP/nvim-gps",
+			requires = {
+				"nvim-treesitter/nvim-treesitter",
+				"windwp/nvim-autopairs",
+			},
+		})
 
-    use {
-      'nvim-lualine/lualine.nvim',
-      config = require('ui.lualine').config,
-      requires = {
-        'kyazdani42/nvim-web-devicons',
-        opt = true,
-      },
-    }
+		use({
+			"nvim-lualine/lualine.nvim",
+			config = require("ui.lualine").config,
+			requires = {
+				"kyazdani42/nvim-web-devicons",
+				opt = true,
+			},
+		})
 
-    -- helper
-    use { 'editorconfig/editorconfig-vim' } -- editorconfig, for indent
-    use { 'tpope/vim-repeat' }
-    use {
-      'folke/which-key.nvim',
-      config = require('helper.whichkey').config,
-    }
+		-- helper
+		use({ "editorconfig/editorconfig-vim" }) -- editorconfig, for indent
+		use({ "tpope/vim-repeat" })
+		use({
+			"folke/which-key.nvim",
+			config = require("helper.whichkey").config,
+		})
 
-    -- editor
-    use { 'tpope/vim-commentary' }
-    -- use { 'tpope/vim-surround' }
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      -- opt = true,
-      -- event = {
-      --   'BufRead',
-      -- },
-      run = ':TSUpdate',
-      config = require('editor.treesitter').config,
-    }
+		-- editor
+		use({ "tpope/vim-commentary" })
+		-- use { 'tpope/vim-surround' }
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			-- opt = true,
+			-- event = {
+			--   'BufRead',
+			-- },
+			run = ":TSUpdate",
+			config = require("editor.treesitter").config,
+		})
 
-    -- completion
-    use {
-      'hrsh7th/nvim-cmp',
-      config = function()
-        require('completion.cmp')
-      end,
-      requires = {
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'rafamadriz/friendly-snippets',
-      },
-      disable = false,
-    }
+		-- completion
+		use({
+			"hrsh7th/nvim-cmp",
+			config = function()
+				require("completion.cmp")
+			end,
+			requires = {
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-cmdline",
+				"L3MON4D3/LuaSnip",
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+			disable = false,
+		})
 
-    use {
-      'hrsh7th/cmp-nvim-lsp',
-    }
+		use({
+			"hrsh7th/cmp-nvim-lsp",
+		})
 
+		use({
+			"junnplus/nvim-lsp-setup",
+			requires = {
+				"williamboman/nvim-lsp-installer",
+				"folke/lua-dev.nvim",
+				"neovim/nvim-lspconfig",
+				"ray-x/lsp_signature.nvim",
+				"RRethy/vim-illuminate",
+			},
+			config = require("completion.lsp-setup").config,
+		})
 
-    use {
-      'junnplus/nvim-lsp-setup',
-      requires = {
-        "williamboman/nvim-lsp-installer",
-        "folke/lua-dev.nvim",
-        "neovim/nvim-lspconfig",
-        'ray-x/lsp_signature.nvim',
-        'RRethy/vim-illuminate',
-      },
-      config = require('completion.lsp-setup').config,
-    }
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"nvim-lua/popup.nvim",
+			},
+			config = function()
+				require("helper.telescope")
+			end,
+		})
 
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = {
-        'nvim-lua/plenary.nvim',
-        'nvim-lua/popup.nvim',
-      },
-      config = function()
-        require('helper.telescope')
-      end,
-    }
+		-- Motions
+		use({ "andymass/vim-matchup", event = "CursorMoved" })
+		use({ "chaoren/vim-wordmotion" })
+		use({ "wellle/targets.vim", event = "CursorMoved" })
+		use({ "unblevable/quick-scope", disable = false })
 
-    -- Motions
-    use { "andymass/vim-matchup", event = "CursorMoved" }
-    use { "chaoren/vim-wordmotion" }
-    use { "wellle/targets.vim", event = "CursorMoved" }
-    use { "unblevable/quick-scope", disable = false }
+		use({
+			"phaazon/hop.nvim",
+			cmd = { "HopWord", "HopChar1" },
+			config = function()
+				require("hop").setup({})
+			end,
+		})
 
-    use {
-      "phaazon/hop.nvim",
-      cmd = { "HopWord", "HopChar1" },
-      config = function()
-        require("hop").setup {}
-      end,
-    }
+		use({
+			"windwp/nvim-autopairs",
+			requires = {
+				"nvim-cmp",
+			},
+			config = function()
+				require("editor.autopairs")
+			end,
+		})
 
-    use {
-      'windwp/nvim-autopairs',
-      requires = {
-        "nvim-cmp",
-      },
-      config = function()
-        require('editor.autopairs')
-      end,
-    }
+		use({
+			"jose-elias-alvarez/null-ls.nvim",
+		})
 
+		if packer_bootstrap then
+			print("Restart Neovim required after installation!")
+			require("packer").sync()
+		end
+	end
 
-    if packer_bootstrap then
-      print "Restart Neovim required after installation!"
-      require("packer").sync()
-    end
-  end
+	packer_init()
 
-  packer_init()
-
-  local packer = require "packer"
-  packer.init(conf)
-  packer.startup(plugins)
+	local packer = require("packer")
+	packer.init(conf)
+	packer.startup(plugins)
 end
 
 return M
