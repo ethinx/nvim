117f479275b545280c10888e5d56af78fbd1d463
York Wong <eth2net@gmail.com>
2022-06-17 16:42:56 +0800

    close quickfix

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -5,3 +5,7 @@ api.nvim_create_autocmd(
     "BufReadPost",
     { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
+
+-- close quickfix
+vim.cmd("autocmd FileType qf nnoremap <buffer> <CR>  <CR>:cclose<CR>")
+vim.cmd("autocmd FileType qf nnoremap <buffer> <ESC> :cclose<CR>")
