7b46cd99053a1c9c76ce80789d227e47e46fc96c
York Wong <eth2net@gmail.com>
2022-08-17 16:07:57 +0800

    set beancount ft

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -2,10 +2,14 @@ local api = vim.api
 
 -- go to last loc when opening a buffer
 api.nvim_create_autocmd(
-    "BufReadPost",
-    { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
+  "BufReadPost",
+  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
 
 -- close quickfix
 vim.cmd("autocmd FileType qf nnoremap <buffer> <CR>  <CR>:cclose<CR>")
 vim.cmd("autocmd FileType qf nnoremap <buffer> <ESC> :cclose<CR>")
+
+vim.cmd([[
+  au BufNewFile,BufRead *.bean,*.beancount set filetype=beancount
+]])
